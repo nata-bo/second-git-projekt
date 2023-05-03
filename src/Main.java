40ab59d70aa4d18bfa404f64553c5d616b6d54b5
Natalia Bodnaryuk <bodnata.o@gmail.com>
2023-05-03 13:40:28 +0200

    add new code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Phone phone1 = new Phone("Samsung", 4, "Android", 5.2);
+        //   printPhone(phone1);
+        Phone phone2 = new Phone("Apple", 4, "iOS", 4.7);
+        //   printPhone(phone2);
+        Phone phone3 = new Phone("Lg", 8, "Android", 6.2);
+        //    printPhone(phone3);
+
+        Phone[] phones = new Phone[]{phone1, phone2, phone3};
+        for (int i = 0; i < phones.length; i++) {
+            printPhone(phones[i]);
+            //  System.out.println(phones[i]);- так напечатает тоько ссылки к адресу
+        }
+
+    }
+
+    public static void printPhone(Phone phone) {
+        System.out.println("--------- печатаем телефон --------");
+        System.out.println("Brand:" + phone.brand);
+        System.out.println("Os:" + phone.os);
+        System.out.println("Ram:" + phone.ram);
+        System.out.println("screenSize:" + phone.screenSize);
+
     }
 }
