6886db6f2c8c2503772689f553fb4c1f9dc55825
Natalia Bodnaryuk <bodnata.o@gmail.com>
2023-05-03 13:59:56 +0200

    add Phone

---
+++ src/Phone.java
@@ -0,0 +1,15 @@
+public class Phone {
+    String brand;
+    int ram;
+    String os;
+    double screenSize;
+
+    Phone(String brand, int ram, String os, double screenSize) {
+        this.brand = brand;
+        this.ram = ram;
+        this.os = os;
+        this.screenSize = screenSize;
+    }
+
+
+}
